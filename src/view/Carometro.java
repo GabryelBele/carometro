6d17e98a43462450fc9180085432b1e77926a2c5
Gabryel Bele <belegabryel19@gmail.com>
2023-12-07 23:34:17 -0300

    upload image in

---
+++ src/view/Carometro.java
@@ -0,0 +1,199 @@
+package view;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.text.DateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import model.DAO;
+import utils.Validador;
+
+public class Carometro extends JFrame {
+
+	DAO dao = new DAO();
+	private Connection conn;
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JLabel lblStatus;
+	private JLabel lblData;
+	private JLabel lblNewLabel;
+	private JTextField txtRA;
+	private JLabel lblNewLabel_1;
+	private JTextField txtNome;
+	private JLabel lblFoto;
+	private FileInputStream fs;
+	private int tamanho;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Carometro frame = new Carometro();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+
+	public Carometro() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowActivated(WindowEvent e) {
+				status();
+				setarData();
+			}
+		});
+		setTitle("Carômetro");
+		setResizable(false);
+		setIconImage(
+				Toolkit.getDefaultToolkit().getImage(Carometro.class.getResource("/img/instagram_photo_icon.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 640, 356);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.textHighlight);
+		panel.setBounds(0, 263, 626, 56);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		lblStatus = new JLabel("");
+		lblStatus.setIcon(new ImageIcon(Carometro.class.getResource("/img/dboff.png")));
+		lblStatus.setBounds(565, 10, 32, 32);
+		panel.add(lblStatus);
+
+		lblData = new JLabel("");
+		lblData.setHorizontalAlignment(SwingConstants.CENTER);
+		lblData.setForeground(SystemColor.text);
+		lblData.setBackground(SystemColor.menu);
+		lblData.setBounds(10, 10, 545, 32);
+		panel.add(lblData);
+		lblData.setFont(new Font("Tahoma", Font.PLAIN, 14));
+
+		lblNewLabel = new JLabel("RA");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(22, 41, 45, 13);
+		contentPane.add(lblNewLabel);
+
+		txtRA = new JTextField();
+		txtRA.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				String caracteres = "0123456789";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
+		txtRA.setBounds(77, 40, 96, 19);
+		contentPane.add(txtRA);
+		txtRA.setColumns(10);
+		// uso do plain document
+		txtRA.setDocument(new Validador(6));
+
+		lblNewLabel_1 = new JLabel("Nome");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(22, 80, 45, 13);
+		contentPane.add(lblNewLabel_1);
+
+		txtNome = new JTextField();
+		txtNome.setBounds(77, 79, 283, 19);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		// uso do plain document
+		txtNome.setDocument(new Validador(30));
+
+		lblFoto = new JLabel("");
+		lblFoto.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		lblFoto.setIcon(new ImageIcon(Carometro.class.getResource("/img/camera.png")));
+		lblFoto.setBounds(370, 10, 256, 237);
+		contentPane.add(lblFoto);
+
+		JButton btnCarregar = new JButton("Carregar Foto");
+		btnCarregar.setForeground(SystemColor.textHighlight);
+		btnCarregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				carregarFoto();
+			}
+		});
+		btnCarregar.setBounds(219, 124, 141, 27);
+		contentPane.add(btnCarregar);
+	}
+
+	private void status() {
+		try {
+			this.conn = dao.conectar();
+			if (conn == null) {
+				lblStatus.setIcon(new ImageIcon(Carometro.class.getResource("/img/dboff.png")));
+			} else {
+				lblStatus.setIcon(new ImageIcon(Carometro.class.getResource("/img/dbon.png")));
+			}
+			conn.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private void setarData() {
+		Date data = new Date();
+		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
+		lblData.setText(formatador.format(data));
+	}
+
+	private void carregarFoto() {
+		JFileChooser jfc = new JFileChooser();
+		jfc.setDialogTitle("Selecionar arquivos");
+		jfc.setFileFilter(new FileNameExtensionFilter("Arquivos de imagens ( *.PNG, *.JPG, *.JPEG )", "png", "jpg", "jpeg"));
+		int resultado = jfc.showOpenDialog(this);
+		if(resultado == JFileChooser.APPROVE_OPTION) {
+			try {
+				fs = new FileInputStream(jfc.getSelectedFile());
+				tamanho = (int) jfc.getSelectedFile().length();
+				
+				Image foto = ImageIO.read(jfc.getSelectedFile()).getScaledInstance(lblFoto.getWidth(), lblFoto.getHeight(), Image.SCALE_SMOOTH);
+				lblFoto.setIcon(new ImageIcon(foto));
+				lblFoto.updateUI();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+}
+}
